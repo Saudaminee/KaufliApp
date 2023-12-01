0d7053e004c33ff15d08524215ba001a771f889e
Saudaminee <saudamineepandey@virtualemployee.com>
2023-12-01 15:07:19 +0530

    Add staticContents.js to src/utils/

---
+++ src/utils/staticContents.js
@@ -0,0 +1,18 @@
+export default {
+    app_name_txt: 'KaufliApp',
+    login_txt: "Login",
+    logout_user:"Logout",
+    sure_want_logout: "are you sure you want to logout?",
+    signup_new: "New here ? Register",
+    already_login: "Already have an account? SignIn",
+    signup_txt:"SignUp",
+    enter_email:"Enter email id",
+    enter_name:"Enter Name",
+    enter_password:"Enter password",
+    signup_emp:"Your account registered successfully",
+    invalid_manager_id:"Invalid Id !!!!",
+    invalid_email:"That email address is invalid!",
+    already_in_use_email:"That email address is already in use!",
+    enter_mobile_num:"Enter mobile number"
+   
+ }

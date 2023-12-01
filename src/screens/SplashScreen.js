d9ff0077f43646030cb197b35753f4b184ef1b4a
Saudaminee <saudamineepandey@virtualemployee.com>
2023-12-01 14:59:30 +0530

    add splash

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Text, View,StatusBar,Image } from 'react-native'
+import React,{useEffect} from 'react'
+import { IMAGES, getImageFromURL } from "../resources/images"
+import styles from '../utils/styles/styles'
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}) => {
+    useEffect(() => {
+        funcNavigate();
+      }, []);
+      const funcNavigate = async () => {
+        setTimeout(() => {
+            navigation.reset({
+              index: 0,
+              routes: [{ name: "Login" }],
+            });
+        }, 2000);
+      };
+
   return (
-    <View>
-      <Text> SplashScreen </Text>
+    <View style={styles.containerApp}>
+    <StatusBar barStyle={"light-content"} />
+    <View style={{ alignItems: 'center' }}>
+
+      <Image
+        style={{ height: 250, width: 250, resizeMode: 'contain' }}
+        source={getImageFromURL(IMAGES.LOGO)} />
+      <Text style={{ color: 'white', fontSize: 18,marginTop:-20 ,fontWeight:'700'}}>Kaufli</Text>
+
     </View>
+  </View>
   )
 }
 
 export default SplashScreen
-
-const styles = StyleSheet.create({})

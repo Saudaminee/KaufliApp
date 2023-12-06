e365eff2cf64194a954347f285ff94903ce3e759
neha-11111 <132436469+neha-11111@users.noreply.github.com>
2023-12-06 17:04:42 +0530

    6-12

--- src/components/commons/CategoryItem.js
+++ src/components/commons/CategoryItem.js
@@ -1,18 +1,21 @@
 // CategoryItem.js
-import React from 'react';
-import { View, Image,Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { colors } from '../../utils/styles/colors';
-import styles from '../../utils/styles/styles';
+import React from "react";
+import { View, Image, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { colors } from "../../utils/styles/colors";
+import styles from "../../utils/styles/styles";
 
-const CategoryItem = ({ title}) => {
+const CategoryItem = ({ title }) => {
   return (
     <View style={styles.containerCategory}>
       {/* <Image source={image} style={styles.categoryStyle} /> */}
       <TouchableOpacity style={styles.categoryStyle}>
-      <Text style={{textAlign:'center',fontSize:11,color:colors.BLACK}}>{title}</Text>
+        <Text
+          style={{ textAlign: "center", fontSize: 11, color: colors.BLACK }}
+        >
+          {title}
+        </Text>
       </TouchableOpacity>
-     
     </View>
   );
 };
---
+++ src/utils/constant.js
@@ -0,0 +1,8 @@
+export const SIZE = [
+  { id: 1, title: "S" },
+  { id: 2, title: "M" },
+  { id: 3, title: "L" },
+  { id: 4, title: "XL" },
+  { id: 5, title: "XXL" },
+  { id: 6, title: "XXL" },
+];

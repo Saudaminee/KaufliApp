9a7609ab2e3b83371fdc87584296f34030be7578
Neha Sharma <Neha@admins-iMac.local>
2023-12-12 12:16:16 +0530

    by neha

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   Image,
   ScrollView,
-  FlatList,
+  FlatList, ImageBackground
 } from "react-native";
 import React from "react";
 import styles from "../utils/styles/styles";
@@ -55,7 +55,7 @@ const HomeScreen = () => {
       image: getImageFromURL(IMAGES.SHOO),
       name: "4WRD by Dressberry",
       price: 500,
-      
+
       name: "MI Super Bass Bluetooth Wireless Headphones",
       price: "$30",
       shortDetail: "Short detail 2",
@@ -122,183 +122,228 @@ const HomeScreen = () => {
       <Search onSearch={handleSearch} />
       {/* <DiscountCard /> */}
       <ScrollView>
-      <View style={styles.content}>
-        {/* Your app components */}
-        <View>
-          <View  style={{ justifyContent: "space-between", flexDirection: "row" }}>
-            <Text
-              style={{
-                fontSize: 17,
-                color: colors.DARK_BLACK,
-                fontWeight: "800",
-              }}>
-              Categories
-            </Text>
-            <TouchableOpacity
-              style={{ padding: 5, flexDirection: "row" }}
-              onPress={() => navigation.navigate("CategoriesScreen")}
-            >
+        <View style={styles.content}>
+          {/* Your app components */}
+          <View>
+            <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
               <Text
                 style={{
-                  fontSize: 12,
+                  fontSize: 17,
                   color: colors.DARK_BLACK,
-                  marginRight: 5,
-                  alignSelf: "center",
+                  fontWeight: "800",
                 }}>
-                See all
+                Categories
               </Text>
-              <View style={styles.seeAll}>
-                <Image
-                  source={getImageFromURL(IMAGES.NEXT_ICON)}
-                  style={styles.seeAllicon}
-                />
-              </View>
-            </TouchableOpacity>
+              <TouchableOpacity
+                style={{ padding: 5, flexDirection: "row" }}
+                onPress={() => navigation.navigate("CategoriesScreen")}
+              >
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.DARK_BLACK,
+                    marginRight: 5,
+                    alignSelf: "center",
+                  }}>
+                  See all
+                </Text>
+                <View style={styles.seeAll}>
+                  <Image
+                    source={getImageFromURL(IMAGES.NEXT_ICON)}
+                    style={styles.seeAllicon}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+            <ScrollView showsHorizontalScrollIndicator={false} horizontal>
+              {categories.map((category) => (
+                <CategoryItem key={category.id} title={category.title} />
+              ))}
+            </ScrollView>
           </View>
-          <ScrollView showsHorizontalScrollIndicator={false} horizontal>
-            {categories.map((category) => (
-              <CategoryItem key={category.id} title={category.title} />
-            ))}
-          </ScrollView>
-        </View>
 
-        <View>
-          <View
-            style={{
-              justifyContent: "space-between",
-              flexDirection: "row",
-              marginVertical: "1%",
-            }}
-          >
-            <Text
+          <View>
+            <View
               style={{
-                fontSize: 17,
-                color: colors.DARK_BLACK,
-                fontWeight: "800" }} > Brands</Text>
-            <TouchableOpacity
-              style={{ padding: 5, flexDirection: "row" }}
-              onPress={() => navigation.navigate("BrandsScreen")}>
-              <Text
-                style={{
-                  fontSize: 12,
-                  color: colors.DARK_BLACK,
-                  marginRight: 5,
-                  alignSelf: "center" }} > See all </Text>
-              <View style={styles.seeAll}>
-                <Image
-                  source={getImageFromURL(IMAGES.NEXT_ICON)}
-                  style={styles.seeAllicon}
-                />
-              </View>
-            </TouchableOpacity>
-          </View>
-          <ScrollView showsHorizontalScrollIndicator={false} horizontal>
-            {brands.map((brands) => (
-              <BrandsItem
-                key={brands.id}
-                title={brands.title}
-                backgroundColor={getRandomColor()}
-              />
-            ))}
-          </ScrollView>
-        </View>
-
-        <View
-         // style={{ paddingBottom: "32%", marginTop: "2%", marginBottom: "34%" }} 
-          >
-          <View
-            style={{ justifyContent: "space-between", flexDirection: "row" }} >
-            <Text
-              style={{
-                fontSize: 17,
-                color: colors.DARK_BLACK,
-                fontWeight: "800",
-              }} >
-              Flash sale
-            </Text>
-            <TouchableOpacity 
-              style={{ padding: 5, flexDirection: "row" }}
-              onPress={() => navigation.navigate("SaleScreen")} >
-              <Text
-                style={{
-                  fontSize: 12,
-                  color: colors.DARK_BLACK,
-                  marginRight: 5,
-                  alignSelf: "center",
-                }} >
-                See all
-              </Text>
-              <View style={styles.seeAll}>
-                <Image
-                  source={getImageFromURL(IMAGES.NEXT_ICON)}
-                  style={styles.seeAllicon}
-                />
-              </View>
-            </TouchableOpacity>
-          </View>
-          
-          <FlatList
-            data={products}
-            keyExtractor={(item) => item.id.toString()}
-            numColumns={2}
-            renderItem={({ item }) => (
-              <ProductItem
-                name={item.name}
-                image={item.image}
-                price={item.price}
-                shortDetail={item.shortDetail}
-                offer={item.offer}
-                id={item.id}
-                discount={item.discount}
-              />
-            )}
-          />
-        </View>
-
- {/* bottom designs  */}
- <View style={{backgroundColor:'#DACCC4',width:'100%',height:150,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-        <Image source={require('../assets/jacket.png')}
-        style={{width:120,height:120}}
-        />
-
-
-        <View style={{alignItems:'center'}} >
-        <Text style={{textTransform:'capitalize',fontWeight:'bold', color:'#000',fontSize:20,textAlign:'center',maxWidth:'65%'}}>   winter jacket and coats </Text>
-        <View
-              style={{
-                borderRadius: 2,
-                backgroundColor: colors.BLACK,
-                justifyContent: "center",
-                alignItems: "center",
-                width:80,height:30,
-                alignContent:'center',marginTop:10
+                justifyContent: "space-between",
+                flexDirection: "row",
+                marginVertical: "1%",
               }}
             >
-              
               <Text
                 style={{
-                  textTransform: "uppercase",
-                  color: "#fff",
+                  fontSize: 18,
+                  color: colors.DARK_BLACK,
+                  fontWeight: "500"
+                }} > Brands</Text>
+              <TouchableOpacity
+                style={{ padding: 5, flexDirection: "row" }}
+                onPress={() => navigation.navigate("BrandsScreen")}>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.DARK_BLACK,
+                    marginRight: 5,
+                    alignSelf: "center"
+                  }} > See all </Text>
+                <View style={styles.seeAll}>
+                  <Image
+                    source={getImageFromURL(IMAGES.NEXT_ICON)}
+                    style={styles.seeAllicon}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+            <ScrollView showsHorizontalScrollIndicator={false} horizontal>
+              {brands.map((brands) => (
+                <BrandsItem
+                  key={brands.id}
+                  title={brands.title}
+                  backgroundColor={getRandomColor()}
+                />
+              ))}
+            </ScrollView>
+          </View>
+
+          <View
+          // style={{ paddingBottom: "32%", marginTop: "2%", marginBottom: "34%" }} 
+          >
+            <View
+              style={{ justifyContent: "space-between", flexDirection: "row" }} >
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: colors.DARK_BLACK,
                   fontWeight: "500",
-                  fontSize: 12,
-                  textAlign:'center'
+                }} >
+                Flash sale
+              </Text>
+              <TouchableOpacity
+                style={{ padding: 5, flexDirection: "row" }}
+                onPress={() => navigation.navigate("SaleScreen")} >
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.DARK_BLACK,
+                    marginRight: 5,
+                    alignSelf: "center",
+                  }} >
+                  See all
+                </Text>
+                <View style={styles.seeAll}>
+                  <Image
+                    source={getImageFromURL(IMAGES.NEXT_ICON)}
+                    style={styles.seeAllicon}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+
+            <FlatList
+              data={products}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={2}
+              renderItem={({ item }) => (
+                <ProductItem
+                  name={item.name}
+                  image={item.image}
+                  price={item.price}
+                  shortDetail={item.shortDetail}
+                  offer={item.offer}
+                  id={item.id}
+                  discount={item.discount}
+                />
+              )}
+            />
+          </View>
+
+          {/* bottom designs  */}
+          <View style={{ backgroundColor: '#DACCC4', width: '100%', height: 150, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+            <Image source={require('../assets/jacket.png')}
+              style={{ width: 120, height: 120 }}
+            />
+
+
+            <View style={{ alignItems: 'center',marginHorizontal:10,justifyContent:'center' }} >
+              <Text style={{ textTransform: 'capitalize', fontWeight: 'bold', color: '#000', fontSize: 18, }}>winter jacket and coats </Text>
+              <Text style={{ fontSize: 10 }}> Elegant, sporty or casual, choose your style 🧥</Text>
+              <View
+                style={{
+                  borderRadius: 2,
+                  backgroundColor: colors.BLACK,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: 60, height: 25,
+                  alignContent: 'center', marginTop: 10
                 }}
               >
-                {" "}
-                View All 
-              </Text>
-           
+
+                <Text
+                  style={{
+                    textTransform: "uppercase",
+                    color: "#fff",
+                    fontWeight: "500",
+                    fontSize: 10,
+                    textAlign: 'center'
+                  }}
+                >
+                  {" "}
+                  View All
+                </Text>
+
+
+              </View>
+            </View>
+
           </View>
-        </View>
-       
-        </View>
-<Text style={{textTransform:'capitalize',fontWeight:'bold', color:'#000',fontSize:25,maxWidth:'50%',marginTop:10}}>Jacket</Text>
 
-      </View>
 
-     
+          <Text style={{ textTransform: 'capitalize', fontWeight: 'bold', color: '#000', fontSize: 20, maxWidth: '50%', marginVertical: 10 }}> {"{ Jacket }"}</Text>
+
+
+          <View>
+            <ImageBackground
+              resizeMode={"cover"}
+              source={require('../assets/tshirt.jpg')}
+              style={{ height: 120, width: '100%' }}
+            >
+              <Text style={{ textTransform: 'capitalize', fontWeight: 'bold', color: '#fff', fontSize: 18, maxWidth: '95%',marginTop:'10%' ,marginHorizontal:10}}> T-shirts that can't miss in your wardrobe</Text>
+              <Text style={{ fontSize: 10,color:'#fff' ,marginHorizontal:15}}> So many designs for your unique style 👕</Text>
+              <View
+                style={{
+                  borderRadius: 2,
+                  backgroundColor: '#fff',
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: 60, height: 25,
+                  alignContent: 'center', marginTop: 5,
+                  marginHorizontal:15
+                }}
+              >
+
+                <Text
+                  style={{
+                    textTransform: "uppercase",
+                    color: "#000",
+                    fontWeight: "500",
+                    fontSize: 10,
+                    textAlign: 'center'
+                  }}
+                >
+                  {" "}
+                  View All
+                </Text>
+
+
+              </View>
+            </ImageBackground>
+          </View>
+          <Text style={{ textTransform: 'capitalize', fontWeight: 'bold', color: '#000', fontSize: 20, maxWidth: '50%', marginVertical: 10 }}> {"{ t-shirt }"}</Text>
+        </View>
+
+
       </ScrollView>
-     
+
     </SafeAreaView>
   );
 };
